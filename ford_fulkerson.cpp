1d1ab06fb62103a9d1216906cb1562811a444f32
tino <87877385+ajemarobot@users.noreply.github.com>
2024-09-19 21:22:59 +0200

    Update ford_fulkerson.cpp

--- ford_fulkerson.cpp
+++ ford_fulkerson.cpp
@@ -27,25 +27,3 @@ int fordFulkersonDFS(Graph& graph, int source, int sink) {
     return maxFlow;
 }
 
-int fordFulkersonBFS(Graph& graph, int source, int sink) {
-   	vector<vector<int>> residualGraph = graph.adjMatrix;
-    vector<int> parent(graph.V);
-    int maxFlow = 0;
-
-    while (bfs(graph, source, sink, parent, residualGraph)) { 
-        int pathFlow = INT_MAX;
-        for (int v = sink; v != source; v = parent[v]) {
-            int u = parent[v];
-            pathFlow = min(pathFlow, residualGraph[u][v]);
-        }
-
-        for (int v = sink; v != source; v = parent[v]) {
-            int u = parent[v];
-            residualGraph[u][v] -= pathFlow;
-            residualGraph[v][u] += pathFlow;
-        }
-
-        maxFlow += pathFlow;
-    }
-    return maxFlow;
-}
