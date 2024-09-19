9fac35dfd74838e471417429f5c4b2e5a2d3d871
tino <87877385+ajemarobot@users.noreply.github.com>
2024-09-19 20:56:06 +0200

    Create ford_fulkerson.h

---
+++ ford_fulkerson.h
@@ -0,0 +1,10 @@
+/// ford_fulkerson.h
+#ifndef FORD_FULKERSON_H
+#define FORD_FULKERSON_H
+
+#include "graph.h"
+
+int fordFulkersonDFS(Graph& graph, int source, int sink);
+int fordFulkersonBFS(Graph& graph, int source, int sink);
+
+#endif
