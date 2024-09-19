d4278265a279d240011cf90c5a88868d5e022b65
tino <87877385+ajemarobot@users.noreply.github.com>
2024-09-19 21:17:08 +0200

    Create ford_fulkerson.cpp

---
+++ ford_fulkerson.cpp
@@ -0,0 +1,51 @@
+/// ford_fulkerson.cpp
+#include "ford_fulkerson.h"
+#include "search_helpers.h"
+
+using namespace std;
+
+int fordFulkersonDFS(Graph& graph, int source, int sink) {
+    vector<vector<int>> residualGraph = graph.adjMatrix;
+    vector<int> parent(graph.V);
+    int maxFlow = 0;
+
+    while (dfs(graph, source, sink, parent, residualGraph)) {
+        int pathFlow = INT_MAX;
+        for (int v = sink; v != source; v = parent[v]) {
+            int u = parent[v];
+            pathFlow = min(pathFlow, residualGraph[u][v]);
+        }
+
+        for (int v = sink; v != source; v = parent[v]) {
+            int u = parent[v];
+            residualGraph[u][v] -= pathFlow;
+            residualGraph[v][u] += pathFlow;
+        }
+
+        maxFlow += pathFlow;
+    }
+    return maxFlow;
+}
+
+int fordFulkersonBFS(Graph& graph, int source, int sink) {
+   	vector<vector<int>> residualGraph = graph.adjMatrix;
+    vector<int> parent(graph.V);
+    int maxFlow = 0;
+
+    while (bfs(graph, source, sink, parent, residualGraph)) { 
+        int pathFlow = INT_MAX;
+        for (int v = sink; v != source; v = parent[v]) {
+            int u = parent[v];
+            pathFlow = min(pathFlow, residualGraph[u][v]);
+        }
+
+        for (int v = sink; v != source; v = parent[v]) {
+            int u = parent[v];
+            residualGraph[u][v] -= pathFlow;
+            residualGraph[v][u] += pathFlow;
+        }
+
+        maxFlow += pathFlow;
+    }
+    return maxFlow;
+}
