b77ec0b86bd726c47ca8734894f6c3cc27df462d
tino <87877385+ajemarobot@users.noreply.github.com>
2024-09-20 09:18:06 +0200

    Update search_helpers.h

--- search_helpers.h
+++ search_helpers.h
@@ -4,10 +4,14 @@
 
 #include "graph.h"
 #include <vector>
+#include <utility>
 
 using namespace std;
 
 bool bfs(Graph& graph, int source, int sink, vector<int>& parent, vector<std::vector<int>>& residualGraph);
 bool dfs(Graph& graph, int source, int sink, vector<int>& parent, vector<std::vector<int>>& residualGraph);
+
+bool bfsList(int s, int t, vector<int>& parent, vector<vector<pair<int, int>>>& adj, vector<vector<int>>& capacity);
+bool dfsList(int s, int t, vector<int>& parent, vector<vector<pair<int, int>>>& adj, vector<vector<int>>& capacity);
 
 #endif
