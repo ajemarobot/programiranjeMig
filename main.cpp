a922fd59e0fe89bbd08ce3beb8d2f3104fb08770
tino <87877385+ajemarobot@users.noreply.github.com>
2024-09-20 09:23:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,37 @@ int main() {
     graph.addEdgeMatrix(4, 3, 7);
     graph.addEdgeMatrix(4, 5, 4);
 
-    cout << "Max Flow (Ford-Fulkerson DFS): " << fordFulkersonDFS(graph, 0, 5) << endl;
-	cout << "Max Flow (Edmonds-Karp BFS): " << edmondsKarp(graph, 0, 5) << endl;
+    cout << "Max Flow (Ford-Fulkerson Matrix): " << fordFulkersonDFS(graph, 0, 5) << endl;
+	cout << "Max Flow (Edmonds-Karp Matrix): " << edmondsKarp(graph, 0, 5) << endl;
 
-   
+   int nodes = 6; 
+    vector<vector<pair<int, int>>> adj(nodes); 
+    vector<vector<int>> capacity(nodes, vector<int>(nodes, 0)); 
+
+    adj[0].push_back({1, 16});
+    adj[0].push_back({2, 13});
+    adj[1].push_back({2, 10});
+    adj[1].push_back({3, 12});
+    adj[2].push_back({1, 4});
+    adj[2].push_back({4, 14});
+    adj[3].push_back({2, 9});
+    adj[3].push_back({5, 20});
+    adj[4].push_back({3, 7});
+    adj[4].push_back({5, 4});
+
+    capacity[0][1] = 16;
+    capacity[0][2] = 13;
+    capacity[1][2] = 10;
+    capacity[1][3] = 12;
+    capacity[2][1] = 4;
+    capacity[2][4] = 14;
+    capacity[3][2] = 9;
+    capacity[3][5] = 20;
+    capacity[4][3] = 7;
+    capacity[4][5] = 4;
+
+    int source = 0, sink = 5;
+    cout << "Maximum Flow (Ford-Fulkerson List): " << fordFulkersonList(source, sink, adj, capacity) << endl;
+    cout << "Maximum Flow (Edmonds-Karp List): " << edmondsKarpList(source, sink, adj, capacity) << endl;
+    return 0;
+	}
